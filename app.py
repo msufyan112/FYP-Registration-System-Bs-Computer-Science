04600920814a5327c223817dfee42af343f8f4e8
Muhammad Sufyan <156957366+msufyan112@users.noreply.github.com>
2026-04-08 00:22:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ groups_path = os.path.join(current_dir, 'final_groups.csv')
 
 # --- CONFIGURATION ---
 st.set_page_config(page_title="FYP Registration Portal", page_icon="🎓", layout="centered")
-
-
 FORM_NAME = "FYPREGISTRATION"
 
 # --- BACKGROUND & STYLING ---
